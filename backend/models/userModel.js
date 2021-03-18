ed289a8d4a4e70250a8983718abc744e03863074
ysonbrian <znlsakxls@gmail.com>
2021-03-18 19:52:35 +0900

    Modeling Data

---
+++ backend/models/userModel.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+
+const UserSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = moongose.model('User', userSchema);
+
+export default User;
